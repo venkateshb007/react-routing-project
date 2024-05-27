900fc53b72806e9c113ac7eba3d82b44a2253710
Venkatesh B <venkateshb698@gmail.com>
2024-05-27 23:27:53 +0530

    landing-page commit

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -15,7 +15,14 @@ class Home extends React.Component{
     render() {
       return (
         <React.Fragment>
-            
+            <div className="landing-page">
+              <div className="wrapper">
+                <div>
+                  <h2>Welcome to React Routing</h2>
+                  <div className="lead">Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem placeat sunt repellat tempora et dolores dolore ullam, aliquam amet quia nihil autem eius possimus ab! Nam totam ullam minima esse.</div>
+                </div>
+              </div>
+            </div>
         </React.Fragment>
       )
     }
